455c3351ea0f4718533d2dcfc9d9d63239ad4a8d
Carlos Flores Martinez <cfloresm@anzen.com.mx>
2014-02-18 11:37:47 -0600

    Resolve issue for behavior account gruop element .

--- app/scripts/controllers/accounts.js
+++ app/scripts/controllers/accounts.js
@@ -5,20 +5,20 @@ angular.module('SpaApp')
 	$scope.client = 'Ricardo Montemayor Morales';
 
 	 // Get Biometric Accounts
-	$http({
-		url: 'http://mfm.jit.su/api/accounts/1',
-		method: 'GET'
-	}).
-	success(function(data, status, headers) {
-		$scope.biometricAccounts = data;
-		console.log(JSON.stringify($scope.accounts));
+	 $http({
+	 	url: 'http://mfm.jit.su/api/accounts/1',
+	 	method: 'GET'
+	 }).
+	 success(function(data, status, headers) {
+	 	$scope.biometricAccounts = data;
+	 	console.log(JSON.stringify($scope.accounts));
 
-	}).
-	error(function(data, status) {
+	 }).
+	 error(function(data, status) {
 			//put an error message in the scope
 			$scope.errorMessage = 'operation failed';
 
-	});
+		});
 
 	// Get Credit Accounts
 	$http({
@@ -34,26 +34,36 @@ angular.module('SpaApp')
 			//put an error message in the scope
 			$scope.errorMessage = 'operation failed';
 
-	});
+		});
 
 	 // Get Investment Accounts
 
-	$http({
-		url: 'http://mfm.jit.su/api/accounts/3',
-		method: 'GET'
-	}).
-	success(function(data, status, headers) {
-		$scope.investmentAccounts = data;
-		console.log(JSON.stringify($scope.accounts));
+	 $http({
+	 	url: 'http://mfm.jit.su/api/accounts/3',
+	 	method: 'GET'
+	 }).
+	 success(function(data, status, headers) {
+	 	$scope.investmentAccounts = data;
+	 	console.log(JSON.stringify($scope.accounts));
 
-	}).
-	error(function(data, status) {
+	 }).
+	 error(function(data, status) {
 			//put an error message in the scope
 			$scope.errorMessage = 'operation failed';
-	});
+		});
 
-	$scope.logout=function(){
-		$location.path( '/login' );
-	}
+	 $scope.logout=function(){
+	 	$location.path( '/login' );
+	 }
 
+	 //behavior stack accounts group
+	 function show_hide_table( elemento, titulo ){
+	 	if( $(elemento).css('display') == 'block' ){
+	 		$(elemento).hide();
+	 		$(titulo).removeClass('abierto').addClass('cerrado');
+	 	}else{
+	 		$(elemento).show();
+	 		$(titulo).removeClass('cerrado').addClass('abierto');
+	 	}
+	 }
 });
