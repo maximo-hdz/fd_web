640b1b11a7a78a9bb395115c719c50cc87061106
Carlos Flores Martinez <cfloresm@anzen.com.mx>
2014-02-19 10:10:34 -0600

    MFM-26 Implement ui-router module .

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,22 +1,35 @@
 'use strict';
 
-angular.module('SpaApp', [
+angular.module('spaApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngRoute' ,
-])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/login', {
-        templateUrl: 'views/login.html',
-        controller: 'LoginCtrl'
-      })
-      .when('/home', {
-        templateUrl: 'views/accounts.html',
-        controller: 'AccountsCtrl'
-      })
-      .otherwise({
-        redirectTo: '/login'
-      });
+  'ui.router'
+  ])
+.config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
+
+  $urlRouterProvider.otherwise("/login");
+
+  $stateProvider
+  .state('login', {
+    url: '/login',
+    templateUrl: 'views/login.html',
+    controller: 'LoginCtrl'
+  })
+  .state('dashboard', {
+    abstract: true,
+    url: '/',
+    templateUrl: 'views/dashboard.html',
+    controller: 'HomeCtrl'
+  })
+  .state('dashboard.accounts', {
+    url: 'accounts',
+    templateUrl: 'views/accounts.html',
+    controller: 'AccountsCtrl'
+  })
+  .state('dashboard.connection', {
+    url: 'connection',
+    templateUrl: 'views/connection.html',
+    controller: 'ConnectionCtrl'
   });
+});
