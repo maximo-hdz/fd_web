b75895f6dd5883d1ba845c95a83c38e718ce431d
Carlos Flores Martinez <cfloresm@anzen.com.mx>
2014-02-24 18:46:29 -0600

    Modification for url api  biometric-accounts .

--- app/scripts/controllers/biometrics.js
+++ app/scripts/controllers/biometrics.js
@@ -13,31 +13,32 @@ angular.module('spaApp')
 		}
 	}
 
-    $scope.gridOptions = { 	data: 'myData',
-						    multiSelect: false,
-						    selectedItems: $scope.mySelections,
-    						columnDefs: [{field:'_account_id', displayName:'Fecha'}, 
-			        					{field:'account_type', displayName:'Operación'},
-			        					{field:'name', displayName:'Descripcion'},
-			        					{field:'alias', displayName:'Cargo'},
-			        					{field:'currency', displayName:'Abono'},
-			        					{field:'last_digits', displayName:'Saldo'}],
-			        		afterSelectionChange: function(data) {
-								$location.path( $scope.mySelections[0]._account_id+'/detail' );
-							}	
-        				};
+	$scope.gridOptions = { 	data: 'myData',
+	multiSelect: false,
+	selectedItems: $scope.mySelections,
+	columnDefs: [{field:'_account_id', displayName:'Fecha'}, 
+	{field:'account_type', displayName:'Operación'},
+	{field:'name', displayName:'Descripcion'},
+	{field:'alias', displayName:'Cargo'},
+	{field:'currency', displayName:'Abono'},
+	{field:'last_digits', displayName:'Saldo'}],
+	afterSelectionChange: function(data) {
+		$location.path( $scope.mySelections[0]._account_id+'/detail' );
+	}	
+};
 
-	$http({
-		url: 'http://127.0.0.1:9000/accounts/account4.json',
-		method: 'GET'
-	}).
-	success(function(data, status, headers) {
-		$scope.myData = data.accounts;
+
+$http({
+	url: 'http://mfm.jit.su/api/accounts/1',
+	method: 'GET'
+}).
+success(function(data, status, headers) {
+	$scope.myData = data.accounts;
 	 	//console.log('Json: '+JSON.stringify($scope.myData));
-	}).
-	error(function(data, status) {
-		console.log('Error: '+data, status);
-		$location.path( '/login' );
-	});
+	 }).
+error(function(data, status) {
+	console.log('Error: '+data, status);
+	$location.path( '/login' );
+});
 
 });
