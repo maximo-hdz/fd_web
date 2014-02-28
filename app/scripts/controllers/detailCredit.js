d5d9e312acdb4a6086c92e704478196606569114
Alex Bravo R <jbravor@anzen.com.mx>
2014-02-28 09:43:21 -0600

    new view detail

---
+++ app/scripts/controllers/detailCredit.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/* Controllers */
+
+/* This controller is for work flow of inventions, obtain a .json to see on the view invertion.html */
+
+angular.module('spaApp')
+.controller('detailCreditCtrl', ['$scope', '$http','$log',
+  function($scope, $http,$log) {
+    console.log("Controller generate detailCreditCtrl"); 
+    
+   
+  }]);
