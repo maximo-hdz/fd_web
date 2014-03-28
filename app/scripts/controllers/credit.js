cf3cfb946576403fbbe92260b8b61392b4f07c8a
Alex Bravo R <jbravor@anzen.com.mx>
2014-03-28 12:26:52 -0600

    Add navigation detail from view line credit

--- app/scripts/controllers/credit.js
+++ app/scripts/controllers/credit.js
@@ -17,4 +17,37 @@ angular.module('spaApp')
 		$log.error('Error: '+data, status);
 		$location.path( '/login' );
 	});	
+
+
+ $scope.gridOptions ={data: 'myData',
+      columnDefs: [{field:'name',displayName:'Nombre del beneficiario'},
+      {field:'monto',displayName:'Monto'},
+      {field:'acount',displayName:'Cuenta'}]
+    };
+
+    $http({
+      url: $rootScope.restAPIBaseUrl + 'accounts/detailCredit.json',
+      method: 'GET'
+    }).
+    success(function(data, status) {
+        $scope.myData=data.detail;
+    }).
+    error(function(data, status) {
+      //put an error message in the scope
+      $log.error('Error: '+data, status);
+      $scope.errorMessage = 'operation failed';
+    });
+
+   $scope.producto="*******";
+   $scope.noOperacion="#####";
+   $scope.montoEnviar="99,0000.00";
+   $scope.date="DD/MM/YYYY";
+   $scope.tc="00.00";
+   $scope.operacion=45698;
+   $scope.autorizaMas="+ 123859"; 
+   $scope.authorized = [
+      {name:'Rafa MArquez'},
+      {name:'Memo Ochoa'},
+      {name:'Cristiano'}    
+    ];
 });
