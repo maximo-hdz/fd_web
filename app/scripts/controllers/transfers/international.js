cf8e0508468197ef1edb4636a6ca1127c9907739
Carlos Flores Martinez <cfloresm@anzen.com.mx>
2014-04-11 13:05:46 -0500

    Fix for transfers .

---
+++ app/scripts/controllers/transfers/international.js
@@ -0,0 +1,33 @@
+'use strict';
+
+angular.module('spaApp')
+.controller('InternationalCtrl', function($scope, $log) {
+
+$scope.withdrawlAccount="706 58535476";
+$scope.alias="Alias Cuenta";
+$scope.coin="USD/EUR";
+$scope.amount="????";
+$scope.paymentConcept="????";
+$scope.depositAccount="706 58535476";
+$scope.date="14/04/2014";
+$scope.mainName="????";
+$scope.codes="????";
+$scope.bank="????";
+$scope.address="????";
+$scope.country="????";
+$scope.recipientName="????";
+$scope.recipientAddress="????";
+$scope.recipientCity="????";
+$scope.recipientCountry="????";
+$scope.number="+5281 777 0022";
+
+$scope.x=true;
+
+$scope.switch = function(){
+	$scope.x = false;
+	console.log($scope.x);
+}
+
+
+
+});
