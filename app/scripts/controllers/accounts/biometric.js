7500421516b8866355ab4abc811f6deced1e52c8
Carlos Flores Martinez <cfloresm@anzen.com.mx>
2014-04-23 09:43:35 -0500

    MFM-158 Refactor for integration services and providers.

--- app/scripts/controllers/accounts/biometric.js
+++ app/scripts/controllers/accounts/biometric.js
@@ -3,55 +3,66 @@
 angular.module('spaApp')
 .controller('BiometricCtrl', function($scope,$http,$location,ctsBiometricas,$rootScope,$log,$stateParams) {
 
-	/*$scope.biometricAccounts = ctsBiometricas.accounts;
+	$scope.today = function() {
+		$scope.dateFrom = new Date();
+		$scope.dateTo = new Date();
+	};
 
-	.controller('BiometricsCtrl', function($scope,$http,$location,$sce,$stateParams,ctsBiometricas) {
-
-	$scope.biometricAccounts = ctsBiometricas.accounts;
-
-	$scope.seleccionTMP = {};
-
-	for(var x in $scope.biometricAccounts.accounts){
-		if($stateParams.account_id == $scope.biometricAccounts.accounts[x]._account_id+'#'){
-			$scope.seleccionTMP = $scope.biometricAccounts.accounts[x];
-			break;
-		}
-	}*/
+	$scope.today();
 	$scope.mySelections = [];
-	$rootScope.titulo = 'Saldos - Cuentas Biométricas - Cuenta ####';
 	$scope.gridOptions = {
 		data: 'myData',
 		multiSelect: false,
 		selectedItems: $scope.mySelections,
 		columnDefs: [
-			{field:'_account_id', displayName:'Fecha'},
-			{field:'account_type', displayName:'Operación'},
-			{field:'name', displayName:'Descripcion'},
-			{field:'alias', displayName:'Cargo'},
-			{field:'currency', displayName:'Abono'},
-			{field:'last_digits', displayName:'Saldo'}],
+			{field:'date', displayName:'Fecha'},
+			{field:'operation', displayName:'Operación'},
+			{field:'description', displayName:'Descripcion'},
+			{field:'charge', displayName:'Cargo'},
+			{field:'payment', displayName:'Abono'},
+			{field:'balance', displayName:'Saldo'}],
 
 		afterSelectionChange: function(data) {
-				$location.path( $scope.mySelections[0]._account_id+'/detailMovement' );
+				console.log($scope.mySelections[0].transaction_id);
+				$location.path("accounts/biometric/transacction/" + $scope.mySelections[0].transaction_id);
 			}
-
-
 	};
 
-	$scope.amount=100000;
-	$scope.from="06/03/2014";
-	$scope.to="20/03/2014";
-
+	//TODO: Get temporal json for transacctions
 	$http({
-		url: $rootScope.restAPIBaseUrl + 'accounts/1',
+		url: 'json/biometric-transacctions.json',
 		method: 'GET'
 	}).
 	success(function(data, status, headers) {
-		$scope.myData = data.accounts;
+		$scope.myData = data.transacctions;
 	}).
 	error(function(data, status) {
 		$log.error('Error: '+data, status);
 		$location.path( '/login' );
 	});
 
+
+	$scope.accountNumber = $stateParams.account_id;
+	$scope.amount=100000;
+	$scope.from="06/03/2014";
+	$scope.to="20/03/2014";
+  	$scope.format = 'dd/MM/yyyy';
+	/** functions for datepicker **/
+
+	// Disable weekend selection
+	$scope.disabled = function(date, mode) {
+	 	return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+	};
+
+	$scope.openFrom = function($event) {
+	    $event.preventDefault();
+	    $event.stopPropagation();
+	    $scope.openedfrom = true;
+	}
+
+	$scope.openTo = function($event) {
+	    $event.preventDefault();
+	    $event.stopPropagation();
+	    $scope.openedto = true;
+	};
 });
