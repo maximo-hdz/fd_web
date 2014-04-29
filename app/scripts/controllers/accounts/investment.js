8a0dfa62281bd1f68dca926d6ce3d7bbfb81eb5b
Víctor <vmartinez@anzen.com.mx>
2014-04-28 21:29:51 -0500

    MFM-163 Investment Balance

--- app/scripts/controllers/accounts/investment.js
+++ app/scripts/controllers/accounts/investment.js
@@ -4,7 +4,7 @@
  * This controller is  work flow of inventions, obtain a .json to see on the view invertion.html
  */
 angular.module('spaApp')
-.controller('InvestmentCtrl',function($scope,$http,$location,$rootScope,$log) {
+.controller('InvestmentCtrl',function($scope,$http,$location,$rootScope,$log, investmentProvider) {
 	
 	$scope.moneda = [
 	    {name:'MXN - Pesos '},
@@ -13,20 +13,14 @@ angular.module('spaApp')
   	$scope.tipoDivisa = $scope.moneda[0]; 
 
 
+  	investmentProvider.getInvestmentAccounts().then(function(){
+  		console.log('ok1');
 
-	$http({
-		url:'json/data-investment.json',
-		method: 'GET'
-	}).
-		success(function(data, status, headers) {
-
-		$scope.myData = data.transacctions;
-	}).
-		error(function(data, status) {
-		//put an error message in the scope
-		$log.error('Error: '+data, status);
-		$scope.errorMessage = 'operation failed';
+  		$scope.myData = $rootScope.investAccounts;
+  	
 	});
+
+
 
 	$scope.gridOptions = {
 
