3f699779e608521e94efab7f32e0352e867fcd23
Carlos Flores Martinez <cfloresm@anzen.com.mx>
2014-04-30 19:08:25 -0500

    MFM-173 Create directive for Navigation bar .

--- app/scripts/directives/bar-navigation.js
+++ app/scripts/directives/bar-navigation.js
@@ -1,14 +1,20 @@
 'use strict';
 
-angular.module('spaApp').directive('navigationBar', function() {
+angular.module('spaApp').directive('navigationBar', function($location) {
   return {
     restrict: 'E',
     replace: true,
     priority: 100,
-    templateUrl: 'views/directives/bar-navigation.html',
-    link: function($scope) {
-      $scope.$on('$stateChangeSuccess', function(){
-      });
-    }
+    scope: {},
+    controller: function($scope, $element) {
+        $scope.getClass = function(path) {
+			if ($location.path().substr(0, path.length) == path) {
+				return 'active'
+			} else {
+				return ""
+			}
+		}
+    },
+    templateUrl: 'views/directives/bar-navigation.html'
   };
 });
