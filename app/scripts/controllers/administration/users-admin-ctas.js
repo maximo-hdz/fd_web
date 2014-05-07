7338d3e263528d55e631ca1ea396900d46975018
Víctor <vmartinez@anzen.com.mx>
2014-05-07 16:17:15 -0500

    MFM-196 Create Directive for left Administration Controllers

--- app/scripts/controllers/administration/users-admin-ctas.js
+++ app/scripts/controllers/administration/users-admin-ctas.js
@@ -3,46 +3,8 @@
 angular.module('spaApp')
 .controller('UsersAdministrationCtasCtrl', function ($rootScope,$scope,$http,$location,$sce,$stateParams) {
 
-
-
-
-
-
 $scope.specialist =  'David Torres Fernandez';
 	$rootScope.titulo = 'Administracion';
-
-
-	$scope.load_conection = function () {
-		
-		
-		$('.page_conection').css({
-			'opacity': '0'
-		});
-		setTimeout(function(){
-
-			$('.page_conection').animate({
-				'opacity': '1'
-			},300);
-		},500);
-
-		$( $('.conexion_btn').get().reverse() ).each(function( index ){
-			var $this = $(this);
-			setTimeout(function(){
-				$this.css({'height':'100px', 'width': '60%' });
-				$this.removeClass('desactivo').addClass('activo');
-				$this.find('i').removeClass('oculto').addClass('visible');
-				
-
-
-			}, ((index + 1) * 400) );
-		});
-
-	
-	};
-
-
-	
-
 });
 
 
@@ -52,7 +14,6 @@ angular.module('spaApp')
 $scope.redirect = function(){
 	
 $location.path("/administration/accounts/");	
-
 
 }
 
