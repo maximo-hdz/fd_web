7b7a3aaf9177104b15b47c960094af88cd73d9e0
Carlos Flores Martinez <cfloresm@anzen.com.mx>
2014-05-22 11:25:02 -0500

    Add reverse proxy url , and token session configuration .

--- app/scripts/services/api.js
+++ app/scripts/services/api.js
@@ -10,13 +10,12 @@ angular.module('spaApp').factory('api', ['$http', '$rootScope', function ($http,
     init: function (token) {
       $rootScope.isAuthenticated = false;
       // this is the token of the bank
-      $http.defaults.headers.common['X-BANK-TOKEN'] = 3;
       $http.defaults.headers.common['X-AUTH-TOKEN'] = token || $rootScope.session_token;
 
       console.log("Executes init & token = " + $rootScope.session_token);
     },
     config: function(){
-      $rootScope.restAPIBaseUrl = 'http://projects.anzen.com.mx:8081/mfm/api/v1/';
+      $rootScope.restAPIBaseUrl = 'http://mfm-api.anzen.com.mx/mfm/api/v1/';
     }
   };
 }]);
