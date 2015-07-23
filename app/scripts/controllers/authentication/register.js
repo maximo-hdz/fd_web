4e079751df2c0e7c8393406424392d4dbb0531fa
Armando Máximo Hernández <ahernandezs@anzen.com.mx>
2015-07-23 10:40:31 -0500

    validations

--- app/scripts/controllers/authentication/register.js
+++ app/scripts/controllers/authentication/register.js
@@ -10,19 +10,26 @@ angular.module('spaApp')
 	$scope.dataRegister = {};
 
 	$scope.register = function(step){
-
-		//todo mejorar esta validación
+		//TODO Cambiar alerts por otra cosa
 		if(step === 1){
 			if(typeof $scope.dataRegister.password == 'undefined' || typeof $scope.dataRegister.new_password == 'undefined' || typeof $scope.dataRegister.confirm_password == 'undefined' ){
 				$window.alert('Favor de llenar todos los campos');
-			}else{
-				if($scope.dataRegister.new_password !== $scope.dataRegister.confirm_password){
-					$window.alert('Las contraseñas no coinciden');
-				}else{
-					$scope.selection = step;
-				}
+				return;
 			}
+			if($scope.dataRegister.new_password !== $scope.dataRegister.confirm_password){
+				$window.alert('Las contraseñas no coinciden');
+				return;
+			}
+			$scope.selection = step;
 		}else{
+			if(typeof $scope.dataRegister.saludo == 'undefined'){
+				$window.alert('Ingresa tu saludo secreto');
+				return;
+			}
+			if(typeof $scope.dataRegister.image_id == 'undefined'){
+				$window.alert('Selecciona una imagen');
+				return;
+			}
 			$scope.selection = step;
 		}
 	}
@@ -33,10 +40,35 @@ angular.module('spaApp')
 
 	$scope.selectImage = function(id){
 		$scope.dataRegister.image_id = id;
-		console.log('imagen seleccionada '+$scope.dataRegister.image_id )
 	}
 
 	$scope.signup = function(){
+
+		if(typeof $scope.dataRegister.question1 == 'undefined'){
+			$window.alert('Selecciona la pregunta 1');
+			return;
+		}
+		if(typeof $scope.dataRegister.response1 == 'undefined'){
+			$window.alert('Ingresa la respuesta a la pregunta 1');
+			return;
+		}
+		if($scope.dataRegister.question2 === $scope.dataRegister.question1 ){
+			$window.alert('Selecciona dos preguntas distintas');
+			return;
+		}
+		if(typeof $scope.dataRegister.question2 == 'undefined'){
+			$window.alert('Selecciona la pregunta 2');
+			return;
+		}
+		if(typeof $scope.dataRegister.response2 == 'undefined'){
+			$window.alert('Ingresa la respuesta a la pregunta 2');
+			return;
+		}
+		if(typeof $scope.dataRegister.terms == 'undefined' ){
+			$window.alert('Acepta los términos y condiciones');
+			return;
+		}
+
 		var new_condition_action = "N";
 		if(typeof $scope.dataRegister['new_condition_action'] !== 'undefined') {
 			if($scope.dataRegister['new_condition_action']){
@@ -44,7 +76,8 @@ angular.module('spaApp')
 			}
 		}
 		authorizeProviderFD.register($scope.data.response.user_login, $scope.dataRegister.password, $scope.dataRegister.new_password, dataAuth.response.with_token, new_condition_action, $scope.dataRegister.image_id, $scope.dataRegister.question1.id, $scope.dataRegister.response1, $scope.dataRegister.question2.id, $scope.dataRegister.response2, $scope.dataRegister.saludo).then(
-			function(result) {
+			function(data) {
+
 				console.log('register succesful');
 				$scope.CheckLogin = true;
 				$rootScope.session_token = result.headers('X-AUTH-TOKEN');
