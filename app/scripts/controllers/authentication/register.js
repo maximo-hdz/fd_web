e99465946a3a087a01bb4ad96b85f4e93870d0fb
Armando Máximo Hernández <ahernandezs@anzen.com.mx>
2015-07-23 00:44:34 -0500

    validación

--- app/scripts/controllers/authentication/register.js
+++ app/scripts/controllers/authentication/register.js
@@ -2,15 +2,29 @@
 
 angular.module('spaApp')
 
-.controller('RegisterCtrl',['$scope', '$rootScope', 'authorizeProviderFD', 'dataAuth', '$location',
-	function($scope, $rootScope, authorizeProviderFD, dataAuth, $location) {
+.controller('RegisterCtrl',['$scope', '$rootScope', 'authorizeProviderFD', 'dataAuth', '$location', '$window',
+	function($scope, $rootScope, authorizeProviderFD, dataAuth, $location, $window) {
 
 	$scope.selection = 0;
 	$scope.data = dataAuth;
 	$scope.dataRegister = {};
 
 	$scope.register = function(step){
-		$scope.selection = step;
+
+		//todo mejorar esta validación
+		if(step === 1){
+			if(typeof $scope.dataRegister.password == 'undefined' || typeof $scope.dataRegister.new_password == 'undefined' || typeof $scope.dataRegister.confirm_password == 'undefined' ){
+				$window.alert('Favor de llenar todos los campos');
+			}else{
+				if($scope.dataRegister.new_password !== $scope.dataRegister.confirm_password){
+					$window.alert('Las contraseñas no coinciden');
+				}else{
+					$scope.selection = step;
+				}
+			}
+		}else{
+			$scope.selection = step;
+		}
 	}
 
 	$scope.logout = function(){
@@ -24,7 +38,7 @@ angular.module('spaApp')
 
 	$scope.signup = function(){
 
-		authorizeProviderFD.register($scope.data.response.user_login, $scope.dataRegister.password, dataAuth.response.with_token, 'N', $scope.dataRegister.image_id, $scope.dataRegister.question1.id, $scope.dataRegister.response1, $scope.dataRegister.question2.id, $scope.dataRegister.response2).then(
+		authorizeProviderFD.register($scope.data.response.user_login, $scope.dataRegister.new_password, dataAuth.response.with_token, 'N', $scope.dataRegister.image_id, $scope.dataRegister.question1.id, $scope.dataRegister.response1, $scope.dataRegister.question2.id, $scope.dataRegister.response2).then(
 			function(data) {
 				console.log('register succesful');
 				$location.path('/logout');
