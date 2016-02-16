3c8649002a18c5aaf0bbf2655eeacd5e302dc8b6
Armando Máximo Hernández <ahernandezs@anzen.com.mx>
2016-02-16 13:17:37 -0600

    cg

--- app/scripts/infrastructure/httpinterceptor.js
+++ app/scripts/infrastructure/httpinterceptor.js
@@ -21,14 +21,14 @@ angular.module('spaApp').factory('httpInterceptor', ['$q', '$location', '$rootSc
         $rootScope.requestStack.pop();
         
         if (!response.status) {
-          console.warn("Response undefined");
+          console.warn('Response undefined');
           timerService.stop();
           $location.url('/login');
         }
 
         if ( response.status === 401 ) {
           $rootScope.session_token = null;
-          console.warn("Status 401");
+          console.warn('Status 401');
           timerService.stop();
           $location.url('/login');
         }
@@ -37,4 +37,4 @@ angular.module('spaApp').factory('httpInterceptor', ['$q', '$location', '$rootSc
       }
     };
 
-}]);
+  }]);
